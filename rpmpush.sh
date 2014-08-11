9e63a7aaf574e7a3a09df43c1f2302452391df20
Nicholas Strugnell <nstrug@redhat.com>
2014-08-11 18:17:02 +0100

    bugfix

--- rpmpush.sh
+++ rpmpush.sh
@@ -22,5 +22,12 @@ fi
 
 rsync -e "ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa" -va \
     ${workdir}/{debug-rpms,rpms,srpms} ${SATELLITE}:
+    
+# use hammer on the satellite to push the RPMs into the repo
+# the ID of the ACME Test repository is 16
+REPO_ID=16
+ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa ${SATELLITE} <<EOF
+hammer repository upload-content --id ${REPO_ID} --path ./rpms
+EOF
 
 
