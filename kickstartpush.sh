ebcb23be612835bb6e0218045ca6c8556af7c904
Nicholas Strugnell <nstrug@redhat.com>
2014-09-16 15:54:27 +0200

    initial import

---
+++ kickstartpush.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+
+# Push Templates to satellite
+#
+# e.g. ${WORKSPACE}/scripts/puppetpush.sh ${WORKSPACE}/soe/kickstarts/
+#
+. ${WORKSPACE}/scripts/common.sh
+
+if [[ -z "$1" ]] || [[ ! -d "$1" ]]
+then
+    echo "Usage: $0 <kickstarts directory>"
+    exit ${NOARGS}
+fi
+workdir=$1
+
+if [[ -z ${PUSH_USER} ]] || [[ -z ${SATELLITE} ]]
+then
+    echo "PUSH_USER or SATELLITE not set or not found"
+    exit ${WORKSPACE_ERR}
+fi
+
+# We delete extraneous kickstarts on the satellite so that we don't keep pushing the same kickstarts into foreman
+rsync --delete -va -e "ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa" -va \
+    ${workdir}/kickstarts/ ${SATELLITE}:kickstarts
+    
+# either update or create each kickstart in turn
+ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa ${SATELLITE} <<EOF
+cd kickstart
+for I in *.erb
+do
+name=$(sed -n 's/^name:\s*\(.*\)/\1/p' ${I})
+id=0
+id=$(hammer --csv template list --per-page 9999 | grep "${name}" cut -d, -f1)
+if [[ ${id} != 0 ]]
+then
+hammer template update --id ${id} ${I}
+else
+type=$(sed -n 's/^kind:\s*\(.*\)/\1/p' ${I})
+hammer template create --file ${I} --name "${name}" --type ${kind}
+fi
+done
+EOF
+
+
