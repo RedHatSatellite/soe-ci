57c5c944f408f598cbc64ed7b81c16a390d88902
Nicholas Strugnell <nstrug@redhat.com>
2014-09-17 16:08:12 +0200

    fixed to use repository synchronisation

--- rpmpush.sh
+++ rpmpush.sh
@@ -18,17 +18,11 @@ then
     echo "PUSH_USER or SATELLITE not set or not found"
     exit ${WORKSPACE_ERR}
 fi
-
-# remove any non-rpm files, as they will confuse hammer if we try to sync them
-find ${workdir}/rpms -type f -a ! -name '*.rpm' -exec rm {} \;
-
-# We delete extraneous RPMs on the satellite so that we don't keep pushing the same RPMs into the repo
-rsync --delete -va -e "ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa" -va \
-    ${workdir}/rpms/ ${SATELLITE}:rpms
     
 # use hammer on the satellite to push the RPMs into the repo
 # the ID of the ACME Test repository is 16
 ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa ${SATELLITE} \
-    "hammer repository upload-content --id ${REPO_ID} --path ./rpms"
+    "hammer repository synchronize --id ${REPO_ID}"
+    
 
 
