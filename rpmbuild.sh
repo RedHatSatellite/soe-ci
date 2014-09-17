d0699ce53152c89d948eacd2f5d9bf23719c4a45
Nicholas Strugnell <nstrug@redhat.com>
2014-09-17 15:54:48 +0200

    fixed to use a local repo for RPMs

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -17,7 +17,7 @@ function build_srpm {
             # determine the name of the rpm from the specfile
             rpmname=$(IGNORECASE=1 awk '/^Name:/ {print $2}' ${SPECFILE})
             rm -f ${WORKSPACE}/artefacts/srpms/${rpmname}-*.src.rpm
-            /usr/bin/mock --buildsrpm --spec ${SPECFILE} --sources $(pwd) --resultdir ${WORKSPACE}/artefacts/srpms
+            /usr/bin/mock --buildsrpm --spec ${SPECFILE} --sources $(pwd) --resultdir ${YUM_REPO}
             RETVAL=$?
             if [[ ${RETVAL} != 0 ]]
             then
@@ -26,26 +26,20 @@ function build_srpm {
             fi
             srpmname=${WORKSPACE}/artefacts/srpms/${rpmname}-*.src.rpm
             rm -f ${WORKSPACE}/artefacts/rpms/${rpmname}-*.{noarch,i386,x86_64}.rpm
-            /usr/bin/mock --rebuild ${srpmname} --resultdir ${WORKSPACE}/artefacts/rpms
+            /usr/bin/mock --rebuild ${srpmname} --resultdir ${YUM_REPO}
             RETVAL=$?
             if [[ ${RETVAL} != 0 ]]
             then
                 echo "Could not build RPM ${rpmname} from ${srpmname}"
                 exit ${RPMBUILD_ERR}
             fi
+            createrepo ${YUM_REPO}
             echo ${git_commit} > .rpmbuild-hash
         else
             echo "No changes since last build - skipping ${SPECFILE}"
         fi
     fi
 }    
-
-# setup artefacts environment 
-rm -f ${WORKSPACE}/artefacts/rpms/*.rpm
-rm -f ${WORKSPACE}/artefacts/srpms/*.src.rpm
-mkdir -p ${WORKSPACE}/artefacts/{rpms,srpms}
-
-
 
 if [[ -z "$1" ]] || [[ ! -d "$1" ]]
 then
