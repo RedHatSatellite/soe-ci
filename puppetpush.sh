47d051bbb056d6606fab3f70a5f84a0582556345
Nicholas Strugnell <nstrug@redhat.com>
2014-09-18 10:17:44 +0200

    fixed to synch puppet modules via pulp repo synchronisation

--- puppetpush.sh
+++ puppetpush.sh
@@ -4,7 +4,7 @@
 #
 # e.g. ${WORKSPACE}/scripts/puppetpush.sh 
 #
-# this should eventually be refactored to use Puppet Force and a repo synch on the satellite
+# this should eventually be refactored to use a Pulp repo sync on the satellite
 # as rpmpush.sh does
 
 . ${WORKSPACE}/scripts/common.sh
@@ -15,12 +15,22 @@ then
     exit ${WORKSPACE_ERR}
 fi
 
-rsync -va -e "ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa" -va \
-    ${PUPPET_REPO}/ ${SATELLITE}:puppet
+# Refresh the PULP_MANIFEST
+cd ${PUPPET_REPO}
+rm PULP_MANIFEST
+touch PULP_MANIFEST
+for I in *.tar.gz
+do
+    size=$(du -b ${I} | awk '{print $1}')
+    sha256=$(sha256sum ${I} | awk '{print $1}')
+    echo "${I},${sha256},${size}" >> PULP_MANIFEST
+done
+
+    
     
 # use hammer on the satellite to push the modules into the repo
 # the ID of the ACME Test repository is 16
 ssh -l ${PUSH_USER} -i /var/lib/jenkins/.ssh/id_rsa ${SATELLITE} \
-    "hammer repository upload-content --id ${PUPPET_REPO_ID} --path ./puppet"
+    "hammer repository synchronize --id ${PUPPET_REPO_ID}"
 
 
