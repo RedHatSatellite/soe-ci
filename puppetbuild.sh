7cd3e40024ef25a51b194d2593e180baed81fca4
David Gurtner <aldavud@crimson.ch>
2014-10-03 16:46:51 +0200

    puppetbuild now uses metadata.json as well as Modulefile
    Modulefile has been deprecated in favor of metadata.json,
    this patch mirrors that change.

--- puppetbuild.sh
+++ puppetbuild.sh
@@ -8,32 +8,32 @@
 . ${WORKSPACE}/scripts/common.sh
 
 function build_puppetmodule {
-    
+
     if [[ -e "$1" ]]
     then
         git_commit=$(git log --format="%H" -1  $(pwd))
         if ! [[ ${git_commit} == $(cat .puppetbuild-hash) ]] 
         then
-            MODULEDIR=$(dirname ${MODULEFILE})
-            modname=$(IGNORECASE=1 awk -F \' '/^name/ {print $2}' ${MODULEFILE})
-            modversion=$(IGNORECASE=1 awk -F \' '/^version/ {print $2}' ${MODULEFILE})
+            MODULEDIR=$(dirname ${METADATA})
+            modname=$(IGNORECASE=1 awk -F \' '/^name/ {print $2}' ${METADATA})
+            modversion=$(IGNORECASE=1 awk -F \' '/^version/ {print $2}' ${METADATA})
             modarchive=${modname}-${modversion}.tar.gz
-        
+
             # build the archive
             puppet module build ${MODULEDIR}
             RETVAL=$?
             if [[ ${RETVAL} != 0 ]]
             then
-                echo "Could not build puppet module ${modname} using the Modulefile ${MODULEFILE}"
+                echo "Could not build puppet module ${modname} using the Modulefile ${METADATA}"
                 exit ${MODBUILD_ERR}
             fi
             mv ${MODULEDIR}/pkg/${modarchive} ${PUPPET_REPO}
             echo ${git_commit} > .puppetbuild-hash
         else
-            echo "No changes since last build - skipping ${MODULEFILE}"
+            echo "No changes since last build - skipping ${METADATA}"
         fi
     fi
-}    
+}
 
 if [[ -z "$1" ]] || [[ ! -d "$1" ]]
 then
@@ -53,11 +53,13 @@ fi
 cd ${workdir}
 for I in $(ls -d */ )
 do
-    MODULEFILE=""
+    METADATA=""
     pushd ${I}
     # find Modulefiles
-    MODULEFILE=$(find $(pwd) -maxdepth 1 -name "Modulefile")
-    if [[ -n ${MODULEFILE} ]] ; then build_puppetmodule ${MODULEFILE} ; fi
+    METADATA=$(find $(pwd) -maxdepth 1 -name "metadata.json")
+    # look for deprecated Modulefile if there is no metadata.json
+    if [[ -n ${METADATA} ]] ; then METADATA=$(find $(pwd) -maxdepth 1 -name "Modulefile") ; fi
+    if [[ -n ${METADATA} ]] ; then build_puppetmodule ${METADATA} ; fi
     popd
 done
 
