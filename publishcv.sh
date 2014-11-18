e1c39d8062f11c46a859b7db17c26cf6c183dadd
Nicholas Strugnell <nstrug@redhat.com>
2014-11-18 18:46:11 +0000

    Added facility to promote CV

--- publishcv.sh
+++ publishcv.sh
@@ -1,10 +1,20 @@
-#!/bin/bash 
+#!/bin/bash -x
 
-# Publish the content view
+# Publish the content view and promote if necessary
 
 . ${WORKSPACE}/scripts/common.sh
 
 ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
     "hammer content-view publish --name \"${CV}\" --organization \"${ORG}\""
 
+if [[ -n ${TESTVM_ENV} ]]
+then
+# get the latest version of the CV and promote it
+    VER=$(ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+    "hammer content-view info --name \"${CV}\" --organization \"${ORG}\" \
+    | grep \"Version:\" | tail -1 | tr -d ' ' | cut -f2 -d ':'")
+    ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+    "hammer content-view promote --content-view \"${CV}\" --organization \"${ORG}\" \
+    --lifecycle-environment \"${TESTVM_ENV}\" --id ${VER}"
+fi
 
