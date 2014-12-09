f364d60f8910a5797b954381d7f5ae36faa6328c
Nicholas Strugnell <nstrug@redhat.com>
2014-12-09 13:10:02 +0100

    changes

--- common.sh
+++ common.sh
@@ -11,18 +11,18 @@ WORKSPACE_ERR=5
 SRPMBUILD_ERR=6
 MODBUILD_ERR=7
 PUSH_USER=jenkins
-SATELLITE=satellite6.acme
-REPO_ID=16
-PUPPET_REPO_ID=17
+SATELLITE=sat6-workshop.lcy.redhat.com
+REPO_ID=10
+PUPPET_REPO_ID=11
 RSA_ID=/var/lib/jenkins/.ssh/id_rsa
 KNOWN_HOSTS=/var/lib/jenkins/.ssh/known_hosts
 TESTVM_HOSTGROUP="Test Servers"
 TESTVM_ENV=2
-TEST_ROOT=1234567890
+TEST_ROOT=r3dhat00
 MODAUTHOR=acme
 YUM_REPO=/var/www/html/pub/soe-repo
 PUPPET_REPO=/var/www/html/pub/soe-puppet
-CV=acme-7.0.0
+CV=7.0.0-ci
 ORG=Default_Organization
 
 
