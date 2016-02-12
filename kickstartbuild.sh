abf4ecf19ccc94955743e58d149cf52f41f071bc
Eric Lavarde <elavarde@redhat.com>
2016-02-12 12:04:30 +0100

    Fix a typo, align message with other *build scripts and use rsync for concision and so that we know what has changed.

--- kickstartbuild.sh
+++ kickstartbuild.sh
@@ -10,22 +10,20 @@
 
 if [[ -z "$1" ]] || [[ ! -d "$1" ]]
 then
-    usage "$0 <directory containing puppet module directories>"
+    usage "$0 <directory containing kickstart files>"
     exit ${NOARGS}
 fi
 workdir=$1
 
 if [[ -z ${WORKSPACE} ]] || [[ ! -w ${WORKSPACE} ]]
 then
-    err "WORKSPACE not set or not found"
+    err "Environment variable 'WORKSPACE' not set or not found"
     exit ${WORKSPACE_ERR}
 fi
 
 # setup artefacts environment 
 ARTEFACTS=${WORKSPACE}/artefacts/kickstarts
-rm -f ${ARTEFACTS}/*.erb
-mkdir -p ${ARTEFACTS}
 
-cp ${workdir}/*.erb ${ARTEFACTS}
-
-
+# copy erb files from one directory to the next, creating directory if needed
+rsync -tdv --delete-excluded --include=*.erb --exclude=* \
+	"${workdir}/" "${ARTEFACTS}"
