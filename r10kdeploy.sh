36f67357171457fdaea319907ba3323aa21a79f0
Nicholas Strugnell <nstrug@redhat.com>
2016-05-09 16:01:20 +0100

    created r10k deployment script

---
+++ r10kdeploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+# Push Puppet Modules out via r10k
+#
+# e.g. ${WORKSPACE}/scripts/r10kdeploy.sh
+#
+ 
+. $(dirname "${0}")/common.sh
+ 
+if [[ -z ${PUSH_USER} ]] || [[ -z ${SATELLITE} ]] || [[ -z ${R10K_USER} ]]
+then
+    err "PUSH_USER, R10K_USER or SATELLITE not set or not found"
+    exit ${WORKSPACE_ERR}
+fi
+ 
+# use hammer on the satellite to push the modules into the repo
+for I in $(ssh -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+    "hammer --csv capsule list" | tail -n +2 | awk -F, '{print $2}')
+do
+    ssh -l ${R10K_USER} -i ${RSA_ID} ${I} \
+        "cd /etc/puppet ; r10k deploy environment ${R10K_ENV} -pv"
+done
