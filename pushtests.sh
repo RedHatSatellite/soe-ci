bde1756784d461343d1265bc23c87d55eace4b68
Patrick C. F. Ernzer <pcfe@pcfe.net>
2016-07-31 16:00:42 +0200

    added test for Sat 6.2

--- pushtests.sh
+++ pushtests.sh
@@ -22,10 +22,25 @@ do
     info "Waiting 10 seconds"
     for I in "${vmcopy[@]}"
     do
-        echo -n "Checking if test server $I has rebuilt... "
+        # cheat and test twice, not enabling --extended-regexp
+        # so this remains portable
+        echo -n "Checking if test server $I has rebuilt... (Sat 6.1 style)"
         status=$(ssh -q -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
             "hammer host info --name $I | \
             grep -e \"Managed.*true\" -e \"Enabled.*true\" -e \"Build.*false\" \
+		| wc -l")
+        # Check if status is OK, ping reacts and SSH is there, then success!
+        if [[ ${status} == 3 ]] && ping -c 1 -q $I && nc -w 1 $I 22
+        then
+            tell "Success!"
+            unset vmcopy[$I]
+        else
+            tell "Not yet."
+        fi
+        echo -n "Checking if test server $I has rebuilt... (Sat 6.2 style)"
+        status=$(ssh -q -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
+            "hammer host info --name $I | \
+            grep -e \"Managed.*yes\" -e \"Enabled.*yes\" -e \"Build.*no\" \
 		| wc -l")
         # Check if status is OK, ping reacts and SSH is there, then success!
         if [[ ${status} == 3 ]] && ping -c 1 -q $I && nc -w 1 $I 22
