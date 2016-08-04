4d56375c00005a4df1a1daffe90a5264036aec45
Nicholas Strugnell <nstrug@redhat.com>
2016-08-04 20:11:56 +0100

    added publish and commit checking functions

--- rpmbuild.py
+++ rpmbuild.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python  -t
+#!/usr/bin/python  -tt
 # Search for RPM source directories and package
 #
 # e.g. ${WORKSPACE}/scripts/rpmbuilder.py ${WORKSPACE}/soe/rpms/ 
@@ -18,18 +18,28 @@ class SRPM:
     def __init__(self,sources,specfile):
         self.sources = sources
         self.specfile = specfile
-        self.srpm_path = None
+        self.srpm_path = None 
+        self.commit = None
+        self.hashfile = self.sources + '/' + '.rpmbuild-hash'
         self.srpms_dir = soeci.WORKSPACE + '/tmp/srpms'
         if not os.path.exists(self.srpms_dir):
             os.makedirs(self.srpms_dir)
 
+    def __codechanged__(self):
+        # create the hashfile if it does not already exist
+        self.commit = subprocess.check_output('git log --format=%%H -1 %s' % self.sources, shell=True)
+        open(self.hashfile,'a').close()
+        h = open('.rpmbuild-hash', 'r')
+        c = h.read()
+        h.close()
+        if c == self.commit:
+            return False
+        else:
+            return True
+
     def build(self):
         os.chdir(self.sources)
-        commit = subprocess.check_output('git log --format=%%H -1 %s' % self.sources, shell=True)
-        # create the hashfile if it does not already exist
-        open('.rpmbuild-hash', 'a').close()
-        hashfile = open('.rpmbuild-hash', 'r+')
-        if commit != hashfile.read():
+        if self.__codechanged__():
             for line in open(self.specfile, 'r'):
                 if 'Name:' in line: rpm_name = line.split()[1]
             for file in glob.glob(self.srpms_dir + '/' + rpm_name + '-*.src.rpm'):
@@ -40,19 +50,19 @@ class SRPM:
                 soeci.stopbuild("Mock SRPM build of %s failed" % (self.root + '/' + self.specfile))
             s = re.search('^Wrote: .*/(.*\.src.rpm)$', m, re.MULTILINE)
             self.srpm_path = self.srpms_dir + '/' + s.group(1)
-            hashfile.seek(0)
-            hashfile.write(commit)
-            hashfile.truncate()
         else:
             print("NO CHANGES SINCE LAST BUILD, SKIPPING %s" % self.specfile)
-        hashfile.close()
-            
+    
+    
+    
         
 class RPM:
     
-    def __init__(self,srpm_path):
+    def __init__(self,srpm_path, hashfile, commit):
         self.srpm_path = srpm_path
         self.rpm_path = None
+        self.hashfile = hashfile
+        self.commit = commit
         self.rpms_dir = soeci.WORKSPACE + '/tmp/rpms'
         if not os.path.exists(self.rpms_dir):
             os.makedirs(self.rpms_dir)
@@ -68,28 +78,40 @@ class RPM:
     def publish(self):
         try:
             shutil.move(self.rpm_path, soeci.YUM_REPO)
-        except:
-            soeci.stopbuild("Could not publish %s into %s" % (self.rpm_path, soeci.YUM_REPO))
+        except shutil.Error as e:
+            soeci.stopbuild("Could not publish %s into %s: %s" % (self.rpm_path, soeci.YUM_REPO, e))
+    
+    def updatehash(self):
+        os.chdir(os.path.dirname(self.srpm_path))
+        print(os.path.dirname(self.srpm_path))
+        h = open(self.hashfile, 'w')
+        h.seek(0)
+        h.write(self.commit)
+        h.truncate()
+        h.close()
         
 def main(argv):
 
     srpms = []
     rpms = []
 
-    for root, dirs, files in os.walk(argv[0]):
-        for file in files:
-            if file.endswith('.spec'):
-                srpms.append(SRPM(root,file))
+    try:
+        for root, dirs, files in os.walk(argv[0]):
+            for file in files:
+                if file.endswith('.spec'):
+                    srpms.append(SRPM(root,file))
+    except:
+        soeci.stopbuild("Could not read input directory")
 
     for s in srpms:
         s.build()
         if s.srpm_path:
-            rpms.append(RPM(s.srpm_path))  
+            rpms.append(RPM(s.srpm_path, s.hashfile, s.commit))  
         
     for r in rpms:
         r.build()
         r.publish()
-
+        r.updatehash()
 
 
 if __name__ == "__main__":
