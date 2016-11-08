80a14f61892b7704469641ef6e2a614f3a4e2053
Patrick C. F. Ernzer <pcfe@redhat.com>
2016-11-08 19:20:28 +0100

    commit c0bac74b0650c6d7b87b4b08838bd859ea3be5bd breaks Sat 6.1, so no need to be 6.1 compat with this either.

--- pushtests.sh
+++ pushtests.sh
@@ -22,21 +22,6 @@ do
     info "Waiting 10 seconds"
     for I in "${vmcopy[@]}"
     do
-        # cheat and test twice, not enabling --extended-regexp
-        # so this remains portable
-        echo -n "Checking if test server $I has rebuilt... (Sat 6.1 style)"
-        status=$(ssh -q -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
-            "hammer host info --name $I | \
-            grep -e \"Managed.*true\" -e \"Enabled.*true\" -e \"Build.*false\" \
-		| wc -l")
-        # Check if status is OK, ping reacts and SSH is there, then success!
-        if [[ ${status} == 3 ]] && ping -c 1 -q $I && nc -w 1 $I 22
-        then
-            tell "Success!"
-            unset vmcopy[$I]
-        else
-            tell "Not yet."
-        fi
         echo -n "Checking if test server $I has rebuilt... (Sat 6.2 style)"
         status=$(ssh -q -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} \
             "hammer host info --name $I | \
