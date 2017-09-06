5a7646a8a3a1ce4604c6ceb8d3723a62068a2b57
Patrick C. F. Ernzer (Red Hat) <pcfe@redhat.com>
2017-09-06 16:52:01 +0000

    first version of capsule sync check

---
+++ capsule-sync-check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# While a Capsule Sync is in progress, we must wait
+# as the System Under Test (SUT) may be behind a Capsule
+
+# Load common parameter variables
+. $(dirname "${0}")/common.sh
+
+if [[ -z ${PUSH_USER} ]] || [[ -z ${SATELLITE} ]]  || [[ -z ${RSA_ID} ]] \
+   || [[ -z ${ORG} ]] || [[ -z ${TESTVM_HOSTCOLLECTION} ]]
+then
+    err "Environment variable PUSH_USER, SATELLITE, RSA_ID, ORG " \
+        "or TESTVM_HOSTCOLLECTION not set or not found."
+    exit ${WORKSPACE_ERR}
+fi
+
+count_capsule_syncs_in_progress()
+{
+  _CAPSULE_SYNC_COUNT=$(ssh -q -l ${PUSH_USER} -i ${RSA_ID} ${SATELLITE} "hammer --csv task list --search 'state = running and label =  Actions::Katello::ContentView::CapsuleGenerateAndSync'" | tail -n +2 | wc -l)
+}
+
+info "Checking if there is a Capsule Sync in progress"
+count_capsule_syncs_in_progress
+if [[ ${_CAPSULE_SYNC_COUNT} -gt 0 ]]
+then
+  _CHEKING_SINCE=$(date)
+  warn "${_CAPSULE_SYNC_COUNT} sync jobs to capsules in progress"
+  while [[ ${_CAPSULE_SYNC_COUNT} -gt 0 ]]
+  do
+    info "Will check again in 1 minute (at $(date --date "1 minute"))"
+    info "We have been checking since      ${_CHEKING_SINCE}"
+    sleep 60
+    count_capsule_syncs_in_progress
+    warn "${_CAPSULE_SYNC_COUNT} sync jobs to capsules still running"
+  done
+else
+  info "No sync jobs to capsules in progress"
+fi
