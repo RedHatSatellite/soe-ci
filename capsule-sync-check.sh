6ddbdf130a4e5a9b932a48bd583c18a2d18311b3
Patrick C. F. Ernzer <pcfe@pcfe.net>
2017-11-21 14:49:08 +0100

    fixes #104

--- capsule-sync-check.sh
+++ capsule-sync-check.sh
@@ -6,6 +6,12 @@
 # Load common parameter variables
 . $(dirname "${0}")/common.sh
 
+
+# how long do we wait
+# 30 minutes is 1800 seconds
+# 4 hours is 14400 seconds
+MAX_SYNC_WAIT=14400
+
 if [[ -z ${PUSH_USER} ]] || [[ -z ${SATELLITE} ]]  || [[ -z ${RSA_ID} ]] \
    || [[ -z ${ORG} ]] || [[ -z ${TESTVM_HOSTCOLLECTION} ]]
 then
@@ -24,6 +30,7 @@ count_capsule_syncs_in_progress
 if [[ ${_CAPSULE_SYNC_COUNT} -gt 0 ]]
 then
   _CHECKING_SINCE=$(date)
+  WAIT=0
   warn "${_CAPSULE_SYNC_COUNT} sync jobs to capsules in progress"
   while [[ ${_CAPSULE_SYNC_COUNT} -gt 0 ]]
   do
@@ -32,6 +39,13 @@ then
     sleep 300
     count_capsule_syncs_in_progress
     warn "${_CAPSULE_SYNC_COUNT} sync jobs to capsules still running"
+    ((WAIT+=300))
+    if [[ ${WAIT} -gt ${MAX_SYNC_WAIT} ]]
+    then
+	    err "Capsules seem to still be syncing after ${MAX_SYNC_WAIT} seconds. Exiting."
+	    err "It is most strongly recommended you look into why your capsules are still syncing after such a long wait."
+	    exit 1
+    fi
   done
 else
   inform "No sync jobs to capsules in progress"
