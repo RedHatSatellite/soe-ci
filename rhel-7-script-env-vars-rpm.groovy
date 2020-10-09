c96a1fdda7acac54b76cd638c3e8f0c14cf28fab
Patrick C. F. Ernzer <pcfe@pcfe.net>
2020-10-09 11:12:50 +0200

    adjusted HC names

--- rhel-7-script-env-vars-rpm.groovy
+++ rhel-7-script-env-vars-rpm.groovy
@@ -1,10 +1,11 @@
 //this is for RHEL7 only as we build packages and shove them in a RHEL7 only yum repo
-env.REPO_ID="10"
-env.PUPPET_REPO_ID="8"
-env.TESTVM_HOSTCOLLECTION="Test Servers Jenkins pipeline"
+env.REPO_ID="3824"
+//env.PUPPET_REPO_ID="8"
+env.TESTVM_HOSTCOLLECTION="hc-soe-el7-test"
+env.GOLDENVM_HOSTCOLLECTION="hc-soe-el7-golden"
 env.YUM_REPO="/var/www/html/pub/soe-repo/rhel7"
 env.PUPPET_REPO="/var/www/html/pub/soe-puppet"
-env.CV="cv-RHEL-Server-Jenkins-pipeline"
+env.CV="cv-soe-ci-el7"
 env.CV_PASSIVE_LIST=""
 env.CCV_NAME_PATTERN=""
 env.CONDITIONAL_VM_BUILD=false
